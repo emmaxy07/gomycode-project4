14ba9ff3dd8c1b9ab5d5eaa3381e8daa3c7e0a09
emmanuel <emmanueliyare7@gmail.com>
2021-10-22 16:48:29 +0100

    app.js file updated

---
+++ src/Profile/Profile.js
@@ -0,0 +1,37 @@
+import PropTypes from "prop-types";
+import React from "react"
+
+const Profile = (props) => {
+  const { fullName, bio, profession } = props
+  const styleObject = {
+    color: 'red',
+    textAlign: 'center'
+  }
+  const handleName = e => {
+    e.preventDefault()
+    alert(fullName)
+  }
+  return(
+    <div style={styleObject}>
+      {props.children}
+      <h2>Full Name: {fullName}</h2>
+      <h2>Bio: {bio}</h2>
+      <h2>Profession: {profession}</h2>
+      <span onClick={handleName}>Click Me</span>
+    </div>
+  )
+}
+
+
+Profile.defaultProps = {
+  fullName: "Emmanuel Omole",
+  bio: "A solution provider",
+  profession: "Software Engineer"
+ };
+
+ Profile.propTypes = {
+ numberProp: PropTypes.number,
+ stringProp: PropTypes.string,
+ }
+
+export default Profile
